196f4ab696d6f1c44b86d9d1d44300386ecdd86e
AlexanderHallCS <hallale21@yahoo.com>
2019-12-13 16:08:17 -0600

    Initial Commit

---
+++ Event Time/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Event Time
+//
+//  Created by Alexander Hall on 12/13/19.
+//  Copyright © 2019 Hall Inc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
