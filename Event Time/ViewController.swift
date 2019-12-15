2e4ad596f842deca17a779868607423bef824c91
AlexanderHallCS <hallale21@yahoo.com>
2019-12-14 21:17:06 -0600

    Start set up of UITableViewController

--- Event Time/ViewController.swift
+++ Event Time/ViewController.swift
@@ -8,13 +8,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class TableViewController: UITableViewController {
 
+    let dateArray = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return dateArray.count
+    }
 
-
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CellName", for: indexPath)
+        cell.textLabel!.text = dateArray[indexPath.row]
+        
+        return cell
+    }
+    
 }
 
