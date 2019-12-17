79d0aeeb9a04d117312a8f49cdcee84ba55adf7f
AlexanderHallCS <hallale21@yahoo.com>
2019-12-17 17:07:02 -0600

    Replace page sheet modal transition with regular push. Add swift file for add reminders view controller.

---
+++ Event Time/AddReminderViewController.swift
@@ -0,0 +1,15 @@
+//
+//  AddReminderViewController.swift
+//  Event Time
+//
+//  Created by Alexander Hall on 12/17/19.
+//  Copyright © 2019 Hall Inc. All rights reserved.
+//
+
+import UIKit
+
+class AddReminderViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
--- Event Time/RemindersTableViewController.swift
+++ Event Time/RemindersTableViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class TableViewController: UITableViewController {
+class RemindersTableViewController: UITableViewController {
 
     let dateArray = [String]()
     override func viewDidLoad() {
